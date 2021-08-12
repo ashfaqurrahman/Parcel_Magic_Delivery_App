0fc9066629232e9dafa7ce293739138ee641e533
Munjur-Alam <munjur533@gmail.com>
2021-08-12 18:05:57 +0600

    order request

---
+++ app/src/main/java/com/airposted/bitoronbd_deliveryman/view/OrderRequestFragment.kt
@@ -0,0 +1,20 @@
+package com.airposted.bitoronbd_deliveryman.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.airposted.bitoronbd_deliveryman.R
+
+class OrderRequestFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order_request, container, false)
+    }
+
+}
